581d0e17896f00de0cde154a27b979c1839dfc3d
Jommy Barban <jommy20bp@gmail.com>
2025-06-07 11:02:49 -0400

    expand dataset with more instances and bosses

--- BattleYellsData.lua
+++ BattleYellsData.lua
@@ -66,6 +66,56 @@ BattleYells = {
       ["Majordomo Executus"] = {"Kneel before the true might!"},
       ["Ragnaros"] = {"By my shout shall you fall, Firelord!"},
       ["__event__"] = {"The very core trembles with our yell!"}
+    },
+    ["Zul'Gurub"] = {
+      ["High Priestess Jeklik"] = {"Silence your screeches!"},
+      ["High Priest Venoxis"] = {"The serpent's fang breaks on my blade!"},
+      ["High Priestess Mar'li"] = {"Your spiders skitter in fear!"},
+      ["High Priest Thekal"] = {"The tiger meets the wolf!"},
+      ["High Priestess Arlokk"] = {"Your panther pounces on nothing!"},
+      ["Bloodlord Mandokir"] = {"I'll show you who owns the arena!"},
+      ["Jin'do the Hexxer"] = {"Your hexes shatter before me!"},
+      ["Hakkar"] = {"The Soulflayer falls today!"},
+      ["__event__"] = {"The drums of Zul'Gurub echo our fury!"}
+    },
+    ["Blackwing Lair"] = {
+      ["Razorgore the Untamed"] = {"Your brood ends before it begins!"},
+      ["Vaelastrasz the Corrupt"] = {"Break your chains and fight!"},
+      ["Broodlord Lashlayer"] = {"I'll cut down your brood!"},
+      ["Firemaw"] = {"Your flames are weak!"},
+      ["Ebonroc"] = {"Darkness cannot shield you!"},
+      ["Flamegor"] = {"Burn for the Horde!"},
+      ["Chromaggus"] = {"Your mutations end here!"},
+      ["Nefarian"] = {"Your reign ends with my shout!"},
+      ["__event__"] = {"Blackwing falls to our warcry!"}
+    },
+    ["Blackrock Depths"] = {
+      ["High Interrogator Gerstahn"] = {"Answer to me, now!"},
+      ["Bael'Gar"] = {"I'll quench your fire!"},
+      ["General Angerforge"] = {"Your anger fuels my rage!"},
+      ["Golem Lord Argelmach"] = {"Your golems crumble before us!"},
+      ["Ambassador Flamelash"] = {"Feel the heat of defeat!"},
+      ["Magmus"] = {"I'll shatter this hall!"},
+      ["Emperor Dagran Thaurissan"] = {"Your empire falls with you!"},
+      ["__event__"] = {"Blackrock will hear our cry!"}
+    },
+    ["Strathholme"] = {
+      ["Baron Rivendare"] = {"Your undead army breaks!"},
+      ["Ramstein the Gorger"] = {"I'll smash you into the pavement!"},
+      ["Baroness Anastari"] = {"Your banshee wails fade!"},
+      ["Magistrate Barthilas"] = {"Justice comes for you!"},
+      ["Balnazzar"] = {"Your corruption ends here!"},
+      ["__event__"] = {"Even the ashes of Strathholme tremble at our yell!"}
+    },
+    ["Scholomance"] = {
+      ["Darkmaster Gandling"] = {"I'll school you in defeat!"},
+      ["Ras Frostwhisper"] = {"Feel the heat of the Horde!"},
+      ["Rattlegore"] = {"I'll rattle your bones!"},
+      ["Lord Alexei Barov"] = {"Your family fortune ends today!"},
+      ["Lady Illucia Barov"] = {"Your dark lessons are over!"},
+      ["Doctor Theolen Krastinov"] = {"No more experiments on us!"},
+      ["The Ravenian"] = {"I'll carve my path through you!"},
+      ["__event__"] = {"Scholomance quakes at our war shout!"}
     }
 }
 
