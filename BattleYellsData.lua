fe6eb26bcb81be0a3eeb64ec0c6fd1b13b0b4fd6
Jommy Barban <jommy20bp@gmail.com>
2025-06-07 10:44:23 -0400

    Expand dataset

--- BattleYellsData.lua
+++ BattleYellsData.lua
@@ -20,6 +20,52 @@ BattleYells = {
       ["__event__"] = {
         "The winds of Durotar howl — and I answer with fury!"
       }
+    },
+    ["Ragefire Chasm"] = {
+      ["Taragaman the Hungerer"] = {"Your hunger ends here!"},
+      ["Bazzalan"] = {"I'll cut you down!"},
+      ["Jergosh the Invoker"] = {"Your dark arts fail you!"},
+      ["Oggleflint"] = {"Rocks crumble before the Horde!"},
+      ["__event__"] = {"The flames of Ragefire answer our shout!"}
+    },
+    ["Wailing Caverns"] = {
+      ["Lady Anacondra"] = {"Fangs won't stop me!"},
+      ["Lord Cobrahn"] = {"I'll crush your serpentine schemes!"},
+      ["Lord Pythas"] = {"Feel the strike of the Horde!"},
+      ["Kresh"] = {"I'll shatter that shell!"},
+      ["Skum"] = {"Thunder lizards fall the hardest!"},
+      ["Lord Serpentis"] = {"Your dreams die today!"},
+      ["Verdan the Everliving"] = {"I'll prune this overgrown weed!"},
+      ["Mutanus the Devourer"] = {"Time to spit you out!"},
+      ["__event__"] = {"The cavern itself trembles at our warcry!"}
+    },
+    ["The Deadmines"] = {
+      ["Rhahk'Zor"] = {"Your mine is our battlefield!"},
+      ["Sneed's Shredder"] = {"I'll smash that toy to bits!"},
+      ["Sneed"] = {"Nowhere to hide, goblin!"},
+      ["Gilnid"] = {"Forge this!"},
+      ["Mr. Smite"] = {"You're not prepared for this!"},
+      ["Captain Greenskin"] = {"I'll send you overboard!"},
+      ["Edwin VanCleef"] = {"For the Horde!"},
+      ["Cookie"] = {"I'll season you with defeat!"},
+      ["__event__"] = {"The Defias will hear our fury!"}
+    },
+    ["Onyxia's Lair"] = {
+      ["Onyxia"] = {"Your reign ends in fire!"},
+      ["__event__"] = {"Dragons fear our battle cry!"}
+    },
+    ["Molten Core"] = {
+      ["Lucifron"] = {"Your corruption burns away!"},
+      ["Magmadar"] = {"Down, beast!"},
+      ["Gehennas"] = {"I'll douse your flames!"},
+      ["Garr"] = {"I'll shatter your binds!"},
+      ["Baron Geddon"] = {"Explode somewhere else!"},
+      ["Shazzrah"] = {"Arcane trickery won't save you!"},
+      ["Sulfuron Harbinger"] = {"Your dark preaching ends!"},
+      ["Golemagg the Incinerator"] = {"Ashes to ashes!"},
+      ["Majordomo Executus"] = {"Kneel before the true might!"},
+      ["Ragnaros"] = {"By my shout shall you fall, Firelord!"},
+      ["__event__"] = {"The very core trembles with our yell!"}
     }
-  }
-  
+}
+
